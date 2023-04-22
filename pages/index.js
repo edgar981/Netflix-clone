3f132ddcb5b9669e39bb35b0bf7afbd9c60d7b72
Edgar <davidnavarro81230@outlook.com>
2023-04-21 21:57:46 -0500

    youtube API json content

--- pages/index.js
+++ pages/index.js
@@ -6,19 +6,12 @@ import Banner from "@/components/banner/banner";
 import NavBar from "@/components/nav/navBar";
 import Card from "@/components/card/card";
 import SectionCards from "@/components/card/section-cards";
+import { getVideos} from "@/lib/videos";
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
-
-    const pelisVideos = [
-        {
-            imgUrl: '/static/upMovie.webp'
-        },
-        {
-            imgUrl: '/static/upMovie.webp'
-        },
-    ]
+    const pelisVideos = getVideos();
 
   return (
     <div className={styles.container}>
