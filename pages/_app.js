60399b5de90f5c463aa8709aeaad510b3c65bb7e
Edgar <davidnavarro81230@outlook.com>
2023-04-29 12:41:00 -0500

    Loading component

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@ import '@/styles/globals.css'
 import {useEffect, useState} from "react";
 import {magicClient} from "@/lib/magic-client";
 import {useRouter} from "next/router";
+import Loading from "@/components/loading/loading";
 
 export default function App({ Component, pageProps }) {
   const router = useRouter();
@@ -34,5 +35,5 @@ export default function App({ Component, pageProps }) {
 
   },[router])
 
-  return isLoading ? <div>Loading...</div> : <Component {...pageProps} />
+  return isLoading ? <Loading /> : <Component {...pageProps} />
 }
