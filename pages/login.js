32ffd15f578173235017627785de09e94589512e
Edgar <davidnavarro81230@outlook.com>
2023-04-29 09:45:46 -0500

    including magic API

--- pages/login.js
+++ pages/login.js
@@ -1,11 +1,32 @@
 import Head from "next/head";
 import styles from "../styles/Login.module.css";
 import Image from "next/image";
+import {useState} from "react";
+import {useRouter} from "next/router";
 
 const Login = () => {
 
+    const router = useRouter();
+    const [email, setEmail] = useState('');
+    const [userMsg, setUserMsg] = useState('');
+
     const handleLoginWithEmail = (e) => {
         e.preventDefault();
+        if (email) {
+            if (email  === 'davidnb81230@gmail.com') {
+                router.push('/')
+            } else {
+                setUserMsg('Something went wrong logging in');
+            }
+        } else {
+            setUserMsg('Enter a valid email address');
+        }
+    }
+
+    const handleOnChangeEmail = (e) => {
+        setUserMsg('');
+        const email = e.target.value;
+        setEmail(email)
     }
 
     return (
@@ -28,8 +49,9 @@ const Login = () => {
                 <div className={styles.mainWrapper}>
                     <h1 className={styles.signinHeader}>Sign In</h1>
 
-                    <input type={'text'} placeholder={'Email address'} className={styles.emailInput}/>
-                    <p className={styles.userMsg}></p>
+                    <input type={'text'} onChange={handleOnChangeEmail} placeholder={'Email address'} className={styles.emailInput}/>
+                    <p className={styles.userMsg}>{userMsg}</p>
+
                     <button onClick={handleLoginWithEmail} className={styles.loginBtn}>Sign in</button>
                 </div>
             </main>
