f80e17165e8ce4ed6964835df32d617c2f3e4265
Edgar <davidnavarro81230@outlook.com>
2023-04-29 12:24:11 -0500

    Redirecting to login page

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,38 @@
 import '@/styles/globals.css'
+import {useEffect, useState} from "react";
+import {magicClient} from "@/lib/magic-client";
+import {useRouter} from "next/router";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const router = useRouter();
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect( () => {
+    const loggingIn = async () => {
+      const isLoggedIn = await magicClient.user.isLoggedIn();
+      if (isLoggedIn) {
+        router.push('/');
+      } else {
+        router.push('/login');
+      }
+    }
+    loggingIn();
+  }, []);
+
+  useEffect(()=> {
+    const handleComplete = () => {
+      setIsLoading(false);
+    }
+
+    router.events.on("routeChangeComplete", handleComplete);
+    router.events.on("routeChangeError", handleComplete);
+
+    return () => {
+      router.events.off("routeChangeComplete", handleComplete);
+      router.events.off("routeChangeError", handleComplete);
+    }
+
+  },[router])
+
+  return isLoading ? <div>Loading...</div> : <Component {...pageProps} />
 }
